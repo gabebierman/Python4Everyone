f72fa365ae417c6e83b6a50eb814dd5dc94806b3
gabebierman <gabebierman@gmail.com>
2022-10-12 10:20:44 -0500

    temp

--- helpme.py
+++ helpme.py
@@ -1,16 +1,15 @@
-from pickle import APPEND
+# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students.
+# Print the average of the marks array for the student name provided, showing 2 places after the decimal.
 
+if __name__ == '__main__':
+    scores = []
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
 
-n = int(input())
-i = 0
-l = []
-
-while i < n:
-    i = i + 1
-    l.append(i)
-
-m = list(map(str, l))
-
-n = ''.join(m)
-
-print(n)
+    av = sum(student_marks[query_name])/len(student_marks[query_name])
+    print("%.2f" % av)
