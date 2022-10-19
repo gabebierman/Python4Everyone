21fa0c143c0a3b2c70484246c3aa6700b3aeefb8
gabebierman <gabebierman@gmail.com>
2022-10-18 19:43:17 -0500

    finish single table assign

--- Chapter_15/EmailBD.py
+++ Chapter_15/EmailBD.py
@@ -1,35 +1,40 @@
 import re
 import sqlite3
 
-conn = sqlite3.connect('emaildb.sqlite')
+conn = sqlite3.connect("emaildb.sqlite")
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS Counts')
 
 cur.execute('''
-CREATE TABLE Counts (email TEXT, count INTEGER)''')
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
-fname = input('Enter file name: ')
+fname = input("Enter file name: ")
 if (len(fname) < 1):
-    fname = 'mbox.txt'
+    fname = "mbox.txt"
 fh = open(fname)
 for line in fh:
-    if not line.startswith('From: '):
+    if not line.startswith("From: "):
         continue
-    pieces = line.split()
-    email = pieces[1]
-    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    pieces = line.split()[1]
+    org = pieces.split("@")[1]
+
+    print(org)
+
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
     row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO Counts (email, count)
-                VALUES (?, 1)''', (email,))
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
     else:
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
-                    (email,))
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org, ))
     conn.commit()
 
 # https://www.sqlite.org/lang_select.html
-sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+print()
 
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
